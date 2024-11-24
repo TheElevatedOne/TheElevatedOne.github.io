8c43898393ba24202607c7687149adbb8c245d5c
Adam Mladý <chickpop24@gmail.com>
2024-11-24 19:26:45 +0100

    sans-serif

--- js/bottom-bar.js
+++ js/bottom-bar.js
@@ -2,8 +2,6 @@ function closeWindow(x) {
     var c = x.id;
     var split = x.id.split("-")[1];
     document.getElementById("widget").style.display = "inline";
-    var csstext = "";
-    document.getElementById(split).style.backgroundColor = "rgba(0, 0, 0, 0)";
     var win = `win-${split}`;
     document.getElementById(win).style.display = "none";
 }
@@ -12,8 +10,6 @@ function openWindow(x) {
     var c = x.id;
     var split = x.id.split("-")[1];
     document.getElementById("widget").style.display = "none";
-    var csstext = "";
-    document.getElementById(split).style.backgroundColor = "rgba(91, 92, 95, 1)";
     var win = `win-${split}`;
     document.getElementById(win).style.display = "flex";
 }
